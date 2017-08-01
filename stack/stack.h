ec52c0bc22dcbfabf94e89a73389ee9af69156d2
Gaoxiang Liu <gaoxiangliu@outlook.com>
2017-08-01 15:59:03 +0800

    Update stack.h

--- stack/stack.h
+++ stack/stack.h
@@ -2,7 +2,6 @@
  *       DataStructures--Stack           *
  *         Experiment program            *
  *        Developed by Sam Liu           *
- *           Not original!!!             *
  * Only for study and education purpose. *
  * The author has no responsibility      *
  *   for the errors or omissions!        *
